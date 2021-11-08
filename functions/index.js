6c8eaeabc13d2d1f3effef1da9a634bf0576fbd0
Tiago Marques <tiago.marques@PT360768MBP16IN4>
2021-11-08 17:52:33 +0000

    Added comments for Runs impl

--- functions/index.js
+++ functions/index.js
@@ -10,15 +10,23 @@ const {
   updateService,
   createService,
   deleteService,
+  runService,
 } = require("./api/services");
 
+// Services
 app.get("/services", getAllServices);
 app.get("/services/:serviceId", getService);
 app.put("/services/:serviceId", updateService);
 app.post("/services", createService);
 app.delete("/services/:serviceId", deleteService);
 
-// app.get("/services/:serviceId/run", runService);
-// app.post("/services/:serviceId/run", createServiceRun);
+// Runs
+// app.get("/services/:serviceId/runs", getAllRuns);
+// app.get("/services/:serviceId/runs/:runId", getRun);
+// app.put("/services/:serviceId/runs/:runId", updateRun);
+// app.post("/services/:serviceId/runs", createRun);
+// app.delete("/services/:serviceId/runs/:runId")
+
+app.get("/services/:serviceId/run", runService);
 
 exports.api = functions.https.onRequest(app);
